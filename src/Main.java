ade23399a6b392a1f451f07960671547f30b818a
DeveSerOGui <guifelixs@gmail.com>
2023-10-12 22:56:43 -0300

    Ajuste final na inicialização da tela

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         int tamanhoLargura = 600;
         int tamanhoAltura = 600;
+
+        JFrame frame = new JFrame("SnakeGame");
+        frame.setVisible(true);
+        frame.setSize(tamanhoLargura, tamanhoAltura);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
